4d0c6e773dd4cc7adbb49bf1f2f75f84ab69b90d
Edwards <micdw@amazon.fr>
2024-09-06 13:36:26 +0200

    adding additional data tables

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -7,12 +7,88 @@ const schema = a
       .model({
         email: a.string(),
         profileOwner: a.string(),
+        children: a.hasMany('Child'),
+        rewardsPunishmentsGifts: a.hasMany('RewardPunishmentGift'),
       })
       .authorization((allow) => [
         allow.ownerDefinedIn("profileOwner"),
       ]),
+
+    Child: a
+      .model({
+        name: a.string(),
+        caretakers: a.manyToMany('UserProfile', 'ChildCaretaker'),
+        rules: a.hasMany('Rule'),
+        timetableItems: a.hasMany('TimetableItem'),
+        pointLogs: a.hasMany('PointLog'),
+      })
+      .authorization((allow) => [
+        allow.owner(),
+        allow.private(),
+      ]),
+
+    ChildCaretaker: a
+      .model({
+        child: a.belongsTo('Child'),
+        caretaker: a.belongsTo('UserProfile'),
+        relationship: a.string(), // e.g., "parent", "grandparent", "uncle"
+      })
+      .authorization((allow) => [
+        allow.owner(),
+        allow.private(),
+      ]),
+
+    Rule: a
+      .model({
+        child: a.belongsTo('Child'),
+        text: a.string(),
+        order: a.integer(),
+      })
+      .authorization((allow) => [
+        allow.owner(),
+        allow.private(),
+      ]),
+
+    TimetableItem: a
+      .model({
+        child: a.belongsTo('Child'),
+        timeslot: a.string(),
+        order: a.integer(),
+        agendaItem: a.string(),
+      })
+      .authorization((allow) => [
+        allow.owner(),
+        allow.private(),
+      ]),
+
+    PointLog: a
+      .model({
+        child: a.belongsTo('Child'),
+        date: a.date(),
+        time: a.time(),
+        points: a.integer(),
+        action: a.string(),
+      })
+      .authorization((allow) => [
+        allow.owner(),
+        allow.private(),
+      ]),
+
+    RewardPunishmentGift: a
+      .model({
+        userProfile: a.belongsTo('UserProfile'),
+        category: a.string(), // "reward", "punishment", or "gift"
+        points: a.integer(),
+        description: a.string(),
+        imageLink: a.string(),
+      })
+      .authorization((allow) => [
+        allow.owner(),
+        allow.private(),
+      ]),
   })
   .authorization((allow) => [allow.resource(postConfirmation)]);
+
 export type Schema = ClientSchema<typeof schema>;
 
 export const data = defineData({
